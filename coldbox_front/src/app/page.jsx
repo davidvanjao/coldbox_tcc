f6ee6e100a6cbb35e0a99076ec5497c1bd88d3a3
BLalves59 <167263927+BLalves59@users.noreply.github.com>
2024-08-06 21:30:26 -0300

    ok
    ok

---
+++ coldbox_front/src/app/page.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className='container'>
+
+      <div className={styles.containerTitulo}>
+        <h1 className={styles.txtTitulo}>Aula next</h1>  
+      </div>
+      
+    </div>
+  )
+}

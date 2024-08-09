c0ec6102507bcc02e2516252b1098cfcb1d46712
BLalves59 <167263927+BLalves59@users.noreply.github.com>
2024-08-08 22:50:09 -0300

    ok
    ok

--- coldbox_front/src/app/landingpage/page.jsx
+++ coldbox_front/src/app/landingpage/page.jsx
@@ -3,16 +3,23 @@ import Image from 'next/image';
 
 
 export default function LandingPage() {
-    return (
-        <header className="header">
-          <nav className="nav">
-            <a href="#" className="link">Início</a>
-            <a href="#" className="link">Soluções</a>
-            <a href="#" className="link">Recursos</a>
-            <a href="#" className="link">Aplicativo</a>
-            <a href="#" className="link">SAC</a>
-            <button className="loginButton">Login</button>
-          </nav>
-        </header>
-      );
-    }
+  return (
+    <>
+      <header className="header">
+        <nav className="nav">
+          <a href="#" className="link">Início</a>
+          <a href="#" className="link">Soluções</a>
+          <a href="#" className="link">Recursos</a>
+          <a href="#" className="link">Aplicativo</a>
+          <a href="#" className="link">SAC</a>
+          <button className="loginButton">Login</button>
+        </nav>
+      </header>
+
+      <div className="imagemFundo">
+        fawsewfewef
+      </div>
+      
+    </>
+  );
+}

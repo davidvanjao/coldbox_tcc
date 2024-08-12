e37fe10f9c36c6b75f06f85c8cf2c020053fe79f
BLalves59 <167263927+BLalves59@users.noreply.github.com>
2024-08-12 20:37:06 -0300

    ok
    ok

--- coldbox_front/src/app/login/login.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './page.css'
-import Image from 'next/image';
-
-
-export default function LandingPage() {
-  return (
-      <h1>tesdte    </h1>
-  );
-}
---
+++ coldbox_front/src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import styles from './page.css'
+import Image from 'next/image';
+
+
+export default function Login() {
+  return (
+    <div className='container2'>
+      <h1>teste</h1>
+    </div>
+  );
+}

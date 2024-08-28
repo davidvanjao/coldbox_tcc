ace45bcc9e05783e81b03661acf1357fa406144c
David Vanjão <35746587+davidvanjao@users.noreply.github.com>
2024-08-28 20:40:31 -0300

    ok
    ok

--- coldbox_mobile/src/pages/Home/styles.js
+++ coldbox_mobile/src/pages/Home/styles.js
@@ -8,7 +8,42 @@ export default styles = StyleSheet.create({
         padding:10
 
     },
-    text: {
-        color: '#000'
+
+    titulo: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        padding:20,
+        textAlign:'center'
     },
+
+    textoSimples: {
+        fontWeight: 'bold',
+        fontSize:15
+    },
+
+    equipamento: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems:'center',
+        
+        backgroundColor: '#CCCCFF',
+        borderRadius:10,
+        padding:10,
+
+        marginTop:10,
+        marginBottom:10
+    },
+
+    equipamentoStatus: {
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'center',
+
+        width: 50,
+        height: 50,
+        borderRadius:10,
+        borderWidth: 2,
+
+    }
 }); 

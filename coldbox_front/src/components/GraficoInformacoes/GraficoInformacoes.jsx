5c83adfe6903c202fbf580cd15cf22d6985dcc6d
BLalves59 <167263927+BLalves59@users.noreply.github.com>
2024-08-29 22:47:46 -0300

    ok
    ok

--- coldbox_front/src/components/GraficoInformacoes/GraficoInformacoes.jsx
+++ coldbox_front/src/components/GraficoInformacoes/GraficoInformacoes.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './GraficoInformacoes.css';
+import camarasAtivosDados from './camarasAtivosDados';
+
 
 const GraficoInformacoes = () => {
   return (
@@ -7,6 +9,32 @@ const GraficoInformacoes = () => {
       <div className='barraCamarasAtivos'>
         <span className='tag'>Câmaras e Ativos</span>
       </div>
+    
+
+    <div className="tabelaCamaras">
+    <table>
+      <thead>
+        <tr>
+          <th>Equipamento</th>
+          <th>Ativo</th>
+          <th>Temp. Interna</th>
+          <th>Umidade</th>
+          <th>Alerta</th>
+        </tr>
+      </thead>  
+      <tbody>
+        {camarasAtivosDados.map((item, index) => (
+          <tr key={index}>
+            <td>{item.equipamento}</td>
+            <td>{item.ativo}</td>
+            <td className={item.alerta ? 'alertaTemp' : ''}>{item.tempInterna}</td>
+            <td>{item.umidade}</td>
+            <td>{item.alerta ? '⚠️' : '✔️'}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    </div>  
     </div>
   );
 };
---
+++ coldbox_front/src/components/GraficoInformacoes/camarasAtivosDados.js
@@ -0,0 +1,33 @@
+const camarasAtivosDados = [
+    {
+      equipamento: 'ESP8266',
+      ativo: 'Câmara Principal',
+      tempInterna: '-1.6°C',
+      umidade: '90%',
+      alerta: false,
+    },
+    {
+      equipamento: 'MEGA2560',
+      ativo: 'Câmara Frios',
+      tempInterna: '-3.0°C',
+      umidade: '92%',
+      alerta: false,
+    },
+    {
+      equipamento: 'ESP8266',
+      ativo: 'Câmara Bebidas',
+      tempInterna: '-2.6°C',
+      umidade: '93%',
+      alerta: false,
+    },
+    {
+      equipamento: 'ESP8266',
+      ativo: 'Câmara Congelados',
+      tempInterna: '-3.4°C',
+      umidade: '95%',
+      alerta: true,
+    },
+  ];
+  
+  export default camarasAtivosDados;
+  

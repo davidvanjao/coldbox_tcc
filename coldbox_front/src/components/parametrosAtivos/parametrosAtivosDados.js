ca9e5a3ac7900811e78ddca5bdd634ecfe54eb61
HENRIQUE MARTINS NEPOMUCENO <159494673+HenriqueMNH@users.noreply.github.com>
2024-09-02 20:35:47 -0300

    Tela de parametros

---
+++ coldbox_front/src/components/parametrosAtivos/parametrosAtivosDados.js
@@ -0,0 +1,33 @@
+const parametrosAtivosDados = [
+    {
+      id: 1,
+      equipamento: 'ESP8266',
+      nomeAtivo: 'Câmara Principal',
+      tempMin: '1.6°C',
+      tempMax: '-2.0°C',
+    },
+    {
+      id: 2,
+      equipamento: 'MEGA2560',
+      nomeAtivo: 'Câmara Frios',
+      tempMin: '3.0°C',
+      tempMax: '-3.0°C',
+    },
+    {
+      id: 3,
+      equipamento: 'ESP8266',
+      nomeAtivo: 'Câmara Bebidas',
+      tempMin: '2.6°C',
+      tempMax: '-3.0°C',
+    },
+    {
+      id: 4,
+      equipamento: 'ESP8266',
+      nomeAtivo: 'Câmara Congelados',
+      tempMin: '3.4°C',
+      tempMax: '-5.0°C',
+    },
+  ];
+  
+  export default parametrosAtivosDados;
+  

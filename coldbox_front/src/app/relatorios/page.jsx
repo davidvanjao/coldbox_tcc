272992e1eee5fef69543e7bbc6162f68676a5df7
BLalves59 <167263927+BLalves59@users.noreply.github.com>
2024-09-02 20:38:14 -0300

    ok
    ok

--- coldbox_front/src/app/relatorios/page.jsx
+++ coldbox_front/src/app/relatorios/page.jsx
@@ -1,23 +1,26 @@
+'use client'
 import Image from 'next/image';
 import Head from 'next/head';
 import NavBar from '../../components/NavBar/NavBar';
 import GoogleChart from '../../components/GoogleChart/GoogleChart';
-// import Barra from '../../components/Barra/barra';
+import Barra from '../../components/Barra/barra';
 import styles from './page.css'
-import Barra from '../../components/Barra/barra'
+import Alerta from '@/components/Alertas/Alertas';
+import CamarasEAtivos from '@/components/CamarasEAtivos/CamarasEAtivos';
 
-export default function relatorios() {
-    return (
-      <div className='bodyGrafico'>
-        <>
-          <NavBar />
-         
-          <Barra/>
-         
-        
-        </>
-        </div>
-      );
+export default function tempoReal() {
+  return (
+    <div className='bodyContainer'>
+      <NavBar />
+      <div className='containerGrid'>
+        <Barra />
+        <GoogleChart />
+        <CamarasEAtivos />
+        <Alerta />
+      </div>
+    </div>
+  );
 }
 
 
+  

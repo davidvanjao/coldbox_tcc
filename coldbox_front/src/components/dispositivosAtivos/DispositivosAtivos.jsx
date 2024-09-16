cde5e77435166d09667ce9411942a7744321d5d2
BLalves59 <167263927+BLalves59@users.noreply.github.com>
2024-09-15 21:15:51 -0300

    ok
    ok

--- coldbox_front/src/components/dispositivosAtivos/DispositivosAtivos.jsx
+++ coldbox_front/src/components/dispositivosAtivos/DispositivosAtivos.jsx
@@ -39,56 +39,54 @@ const DispositivosAtivos = () => {
 
   return (
     <div className={styles.conteinerGrid}>
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <span className={styles.tag}>Dispositivos Ativos</span>
-          <button
-            className={styles.addButton}
-            onClick={() => setShowModal(true)}
-          >
-            <FontAwesomeIcon icon={faPlus} /> Adicionar Dispositivo
-          </button>
-        </div>
-        <div className={styles.tabelaGeral}>
-          <table className={styles.table}>
-            <thead>
-              <tr>
-                <th className={styles.th}>Nome do Dispositivo</th>
-                <th className={styles.th}>Câmara</th>
-                <th className={styles.th}>Data de Instalação</th>
-                <th className={styles.th}>Modelo</th>      {/* Coluna para "Modelo" */}
-                <th className={styles.th}>Observação</th>  {/* Coluna para "Observação" */}
-                <th className={styles.th}>Ações</th>
+      <div className={styles.header}>
+        <span className={styles.tag}>Dispositivos Ativos</span>
+        <button
+          className={styles.addButton}
+          onClick={() => setShowModal(true)}
+        >
+          <FontAwesomeIcon icon={faPlus} /> Adicionar Dispositivo
+        </button>
+      </div>
+      <div className={styles.tabelaGeral}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th className={styles.th}>Nome do Dispositivo</th>
+              <th className={styles.th}>Câmara</th>
+              <th className={styles.th}>Data de Instalação</th>
+              <th className={styles.th}>Modelo</th> {/* Coluna para "Modelo" */}
+              <th className={styles.th}>Observação</th> {/* Coluna para "Observação" */}
+              <th className={styles.th}>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dispositivos.map((item, index) => (
+              <tr
+                key={index}
+                className={index % 2 === 0 ? styles.evenRow : styles.oddRow}
+              >
+                <td className={styles.td}>{item.nomeDispositivo}</td>
+                <td className={styles.td}>{item.nomeCamara}</td>
+                <td className={styles.td}>{item.dataInstalacao}</td>
+                <td className={styles.td}>{item.modelo}</td> {/* Valor "Modelo" */}
+                <td className={styles.td}>{item.observacao}</td> {/* Valor "Observação" */}
+                <td className={styles.td}>
+                  <FontAwesomeIcon
+                    icon={faPen}
+                    className={styles.editIcon}
+                    onClick={() => console.log('Editar dispositivo', index)}
+                  />
+                  <FontAwesomeIcon
+                    icon={faTrash}
+                    className={styles.deleteIcon}
+                    onClick={() => deleteDispositivo(index)}
+                  />
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {dispositivos.map((item, index) => (
-                <tr
-                  key={index}
-                  className={index % 2 === 0 ? styles.evenRow : styles.oddRow}
-                >
-                  <td className={styles.td}>{item.nomeDispositivo}</td>
-                  <td className={styles.td}>{item.nomeCamara}</td>
-                  <td className={styles.td}>{item.dataInstalacao}</td>
-                  <td className={styles.td}>{item.modelo}</td>          {/* Valor "Modelo" */}
-                  <td className={styles.td}>{item.observacao}</td>      {/* Valor "Observação" */}
-                  <td className={styles.td}>
-                    <FontAwesomeIcon
-                      icon={faPen}
-                      className={styles.editIcon}
-                      onClick={() => console.log('Editar dispositivo', index)}
-                    />
-                    <FontAwesomeIcon
-                      icon={faTrash}
-                      className={styles.deleteIcon}
-                      onClick={() => deleteDispositivo(index)}
-                    />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+            ))}
+          </tbody>
+        </table>
       </div>
 
       {showModal && (
@@ -141,13 +139,11 @@ const DispositivosAtivos = () => {
               id="observacao"
               value={newDevice.observacao}
               onChange={handleInputChange}
-              rows="3"
-              required
             />
 
             <div className={styles.modalActions}>
               <button onClick={addDispositivo}>Adicionar</button>
-              <button onClick={() => setShowModal(false)}>Cancelar</button>
+              <button onClick={() => setShowModal(false)}>Fechar</button>
             </div>
           </div>
         </div>
@@ -155,5 +151,7 @@ const DispositivosAtivos = () => {
     </div>
   );
 };
+
+
 
 export default DispositivosAtivos;

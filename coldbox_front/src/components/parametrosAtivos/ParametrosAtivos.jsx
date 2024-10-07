5be213583e3dcade809016c4d5833df1c0761c43
HENRIQUE MARTINS NEPOMUCENO <159494673+HenriqueMNH@users.noreply.github.com>
2024-10-07 20:35:09 -0300

    Update ParametrosAtivos.jsx
    ok

--- coldbox_front/src/components/parametrosAtivos/ParametrosAtivos.jsx
+++ coldbox_front/src/components/parametrosAtivos/ParametrosAtivos.jsx
@@ -2,127 +2,90 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'; 
 import styles from './ParametrosAtivos.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faTrash, faPen } from '@fortawesome/free-solid-svg-icons';
 
 const ParametrosAtivos = () => {
+  const equipId = localStorage.getItem('equip_id');
   const [parametros, setParametros] = useState([]);
-  // const equipId = localStorage.getItem('equip_id');
-  const [allModels, setAllModels] = useState([]); // Para armazenar todos os modelos de equipamentos
+  const [showEditModal, setShowEditModal] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
-  const [currentParametro, setCurrentParametro] = useState(null);
-  const [showModal, setShowModal] = useState(false);
-  const [error, setError] = useState(null);
-  const [equipModelo, setEquipModelo] = useState([]); // Novo estado
+  const [editingItemId, setEditingItemId] = useState(null);
   const [newParameter, setNewParameter] = useState({
-  
-
-
-    equip_id: '', // Alteramos o nome para equip_id
+    equip_id: equipId, 
     param_interface: '',
     param_minimo: '',
     param_maximo: '',
-    data: '',
   });
 
-  // Carregar os dados do banco ao montar o componente
-  useEffect(() => {
-    listarParametro();
-    async function listarParametro() {
-      try {
-        const response = await axios.get('http://127.0.0.1:3333/parametro/1');
-        if (response.data.sucesso) {
-          setParametros(response.data.dados);
-        } else {
-          setError('Erro ao carregar os parametros.');
-        }
-      } catch (error) {
-        setError('Erro ao buscar parametros.');
-      }
-    };
-
-    
-  }
-  );
-
-  // Função para deletar um parâmetro
-  const handleDelete = async (id) => {
+  // Função para listar parâmetros
+  const listarParametro = async () => {
     try {
-      await axios.delete(`http://127.0.0.1:3333/parametro/${id}`);
-      setParametros(parametros.filter((parametros) => parametros.param_id !== id));
+      const response = await axios.get(`http://127.0.0.1:3333/parametro/${equipId}`); 
+      if (response.data.sucesso) {
+        setParametros(response.data.dados);
+      } else {
+        console.error('Erro ao carregar os parâmetros.');
+      }
+    } catch (error) {
+      console.error('Erro ao buscar parâmetros:', error);
+    }
+  };
+
+  useEffect(() => {
+    listarParametro(); // Chama a função listarParametro
+  }, []);
+
+  const handleEdit = (item) => {
+    setNewParameter(item); // Preenche o formulário com os dados do item a ser editado
+    setIsEditing(true); // Define que estamos no modo de edição
+    setEditingItemId(item.param_id); // Guarda o ID do item que está sendo editado
+    setShowEditModal(true); // Abre o modal de edição
+  };
+
+  const handleDelete = async (paramId) => {
+    try {
+      const response = await axios.delete(`http://127.0.0.1:3333/parametro/${paramId}`);
+      if (response.data.sucesso) {
+        // Atualiza a lista de parâmetros após a exclusão
+        setParametros((prevParametros) => prevParametros.filter(param => param.param_id !== paramId));
+      } else {
+        console.error('Erro ao deletar parâmetro');
+      }
     } catch (error) {
       console.error('Erro ao deletar parâmetro:', error);
     }
   };
 
-  // Função para iniciar a edição de um parâmetro
-  const handleEdit = (parametros) => {
-    setCurrentParametro(parametros);
-    setIsEditing(true);
-    setShowModal(true);
-  };
-
-  // Função para iniciar a adição de um novo parâmetro
-  const handleAdd = () => {
-    setIsEditing(false);
-    setShowModal(true);
-  };
-
-  // Função para adicionar um novo parâmetro e enviar para a API
-  const addParameter = async () => {
+  const handleSave = async () => {
     try {
-      const response = await axios.post('http://127.0.0.1:3333/parametro', newParameter);
+      const url = isEditing 
+        ? `http://127.0.0.1:3333/parametro/${editingItemId}` 
+        : 'http://127.0.0.1:3333/parametro';
+      const method = isEditing ? 'put' : 'post';
+      const response = await axios[method](url, newParameter);
+
       if (response.data.sucesso) {
-        setShowModal(false);
-        setParametros([...parametros, response.data.dados]); // Atualiza a lista de parâmetros
-        resetForm();
+        listarParametro(); // Atualiza a lista de parâmetros
+        setShowEditModal(false); // Fecha o modal
+        setIsEditing(false); // Reseta o modo de edição
+        setEditingItemId(null); // Limpa o ID do item que está sendo editado
+        setNewParameter({ // Reseta os campos do novo parâmetro
+          equip_id: equipId, 
+          param_interface: '',
+          param_minimo: '',
+          param_maximo: '',
+        });
       } else {
-        setError('Erro ao adicionar parâmetro');
+        console.error('Erro ao salvar o parâmetro.');
       }
     } catch (error) {
-      setError('Erro ao adicionar parâmetro');
+      console.error('Erro ao salvar o parâmetro:', error);
     }
   };
 
-  // Função para salvar um novo parâmetro ou atualizar um existente
-  const handleSave = async () => {
-    if (isEditing) {
-      try {
-        await axios.put(`http://127.0.0.1:3333/parametro/${currentParametro.param_id}`, currentParametro);
-        setParametros(
-          parametros.map((item) =>
-            item.param_id === currentParametro.param_id ? currentParametro : item
-          )
-        );
-      } catch (error) {
-        console.error('Erro ao editar parâmetro:', error);
-      }
-    } else {
-      addParameter(); // Chama a função de adicionar novo parâmetro
-    }
-    setShowModal(false);
-    resetForm();
-  };
-
-  // Função para capturar as mudanças nos inputs
   const handleChange = (e) => {
     const { name, value } = e.target;
-    if (isEditing) {
-      setCurrentParametro({ ...currentParametro, [name]: value });
-    } else {
-      setNewParameter({ ...newParameter, [name]: value });
-    }
-  };
-
-  // Função para resetar o formulário após adicionar/editar
-  const resetForm = () => {
-    setNewParameter({
-      equip_id: '',
-      param_interface: '',
-      param_minimo: '',
-      param_maximo: '',
-      data: '',
-    });
-    setCurrentParametro(null);
+    setNewParameter((prev) => ({ ...prev, [name]: value }));
   };
 
   return (
@@ -134,8 +97,8 @@ const ParametrosAtivos = () => {
         <thead>
           <tr>
             <th className={styles.th}>ON/OFF</th>
+            <th className={styles.th}>Modelo</th>
             <th className={styles.th}>Equipamento</th>
-            <th className={styles.th}>Nome/Interface</th>
             <th className={styles.th}>Temp. Min</th>
             <th className={styles.th}>Temp. Máx</th>
             <th className={styles.th}>Data</th>
@@ -154,16 +117,10 @@ const ParametrosAtivos = () => {
               <td className={styles.td}>{item.param_maximo}</td>
               <td className={styles.td}>{item.param_data}</td>
               <td className={styles.td}>
-                <button
-                  className={styles.actionButton}
-                  onClick={() => handleEdit(item)}
-                >
-                  <FontAwesomeIcon icon={faEdit} />
+                <button className={styles.actionButton} onClick={() => handleEdit(item)}>
+                  <FontAwesomeIcon icon={faPen} />
                 </button>
-                <button
-                  className={styles.actionButton}
-                  onClick={() => handleDelete(item.param_id)}
-                >
+                <button className={styles.actionButton} onClick={() => handleDelete(item.param_id)}>
                   <FontAwesomeIcon icon={faTrash} />
                 </button>
               </td>
@@ -172,55 +129,49 @@ const ParametrosAtivos = () => {
         </tbody>
       </table>
 
-      {showModal && (
+      {showEditModal && (
         <div className={styles.modal}>
           <div className={styles.modalContent}>
-            <h3>{isEditing ? 'Editar Parâmetro' : 'Novo Parâmetro'}</h3>
-            {error && <p className={styles.error}>{error}</p>}
-            
-            <label htmlFor="equip_id">Modelos de Equipamento</label>
-            <select
-              name="equip_id"
-              value={isEditing ? currentParametro?.equip_id : newParameter.equip_id}
-              onChange={handleChange}
-            >
-              <option value="">Selecione um Equipamento</option>
-              {allModels.map((model) => (
-                <option key={model.equip_id} value={model.equip_modelo}>
-                  {model.equip_modelo}
-                </option>
-              ))}
-            </select>
-
+            <h2>{isEditing ? 'Editar Parâmetro' : 'Adicionar Novo Parâmetro'}</h2>
+            <label htmlFor="param_interface">Equipamento</label>
             <input
               type="text"
               name="param_interface"
-              placeholder="Nome/Interface"
-              value={isEditing ? currentParametro?.param_interface : newParameter.param_interface}
+              id="param_interface"
+              value={newParameter.param_interface}
               onChange={handleChange}
+              required
             />
+            <label htmlFor="param_minimo">Temp. Min</label>
             <input
               type="text"
               name="param_minimo"
-              placeholder="Temp. Min"
-              value={isEditing ? currentParametro?.param_minimo : newParameter.param_minimo}
+              id="param_minimo"
+              value={newParameter.param_minimo}
               onChange={handleChange}
+              required
             />
+            <label htmlFor="param_maximo">Temp. Máx</label>
             <input
               type="text"
               name="param_maximo"
-              placeholder="Temp. Máx"
-              value={isEditing ? currentParametro?.param_maximo : newParameter.param_maximo}
+              id="param_maximo"
+              value={newParameter.param_maximo}
               onChange={handleChange}
-            />
-            <input
-              type="date"
-              name="data"
-              value={isEditing ? currentParametro?.data : newParameter.data}
-              onChange={handleChange}
+              required
             />
             <button className={styles.saveButton} onClick={handleSave}>
-              Salvar
+              {isEditing ? 'Atualizar' : 'Salvar'}
+            </button>
+            <button
+              className={styles.cancelButton}
+              onClick={() => {
+                setShowEditModal(false); // Fecha o modal
+                setIsEditing(false); // Reseta o modo de edição
+                setEditingItemId(null); // Limpa o ID do item que está sendo editado
+              }}
+            >
+              Cancelar
             </button>
           </div>
         </div>

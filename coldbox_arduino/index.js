571a1014f6696e826ad92097fabb7800479b0676
David Vanjão <35746587+davidvanjao@users.noreply.github.com>
2024-10-10 22:38:43 -0300

    ok
    ok

--- coldbox_arduino/index.js
+++ coldbox_arduino/index.js
@@ -19,27 +19,6 @@ app.get('/', (request, response) => {
 
 let n = 0;
 
-const interval = setInterval(() => {
-    n++;
-    //console.log('repete' + ' ' + n);
-    //cadastrar();
-    const dados_temp = parseFloat(lerTemperaturaSimulada().toFixed(2));
-    const dados_umid = lerUmidadeSimulada();
-    const dados_data = lerDataSimulada();
-    //console.log('Temperatura: ', dados_temp);
-    //console.log('Umidade: ', umidAtual);
-    //console.log('Data e hora: ', dataHora);
-
-    novoCadastrar(dados_temp, dados_umid, equip_id, dados_data);
-
-
-
-}, 5000); // 1800000 milissegundos = 30 minutos
-
-
-interval;
-
-
 
 // Parâmetros da simulação
 const temperaturaMedia = 25;   // Temperatura média em °C
@@ -65,15 +44,32 @@ function lerUmidadeSimulada() {
 }
 
 // Função para gerar uma leitura de temperatura simulada
-function lerDataSimulada() {
-    // Obter a data e hora atual
-    const agora = new Date();
-    // Adicionar 30 minutos
-    agora.setMinutes(agora.getMinutes() + 30);
+async function lerDataSimulada(equip_id) {
 
-    return agora;
+    // Definição da instrução SQL de seleção
+    const sql = `select * from novo_equipamento_dados where equip_id = ? order by dados_data desc limit 1`;
+
+    // Definição do parâmetro para o SELECT
+    const values = [equip_id]; // 'equip_id' seria a variável que você quer usar para filtrar os dados
+
+    // Execução da instrução SQL passando o parâmetro
+    const execSql = await db.query(sql, values);
+
+    // O resultado do SELECT estará armazenado em execSql
+    const resultado = execSql[0]; // Pega a primeira linha do resultado
+
+    const dataHora = resultado ? resultado[0].dados_data : null; // Verifica se existe resultado e pega 'dados_data'
+
+    // Adicionar 30 minutos
+    dataHora.setMinutes(dataHora.getMinutes() + 30);
+
+    // Exibe os dados do equipamento
+    //console.log(dataHora);
+
+    return dataHora;
 }
 
+//cadastrar dados do arduino
 async function novoCadastrar(dados_temp, dados_umid, equip_id, dados_data) {
     try {
 
@@ -124,72 +120,6 @@ async function novoCadastrar(dados_temp, dados_umid, equip_id, dados_data) {
     }
 }
 
-
-
-
-
-
-
-
-
-
-async function cadastrar(request, response) {
-    try {
-        // Loop pelos 4 equip_id (de 1 a 4)
-        //for (let equip_id = 1; equip_id <= 5; equip_id++) {
-            const dados_temp = getRandomInRange(-4, 6);
-            const dados_umid = getRandomInRange(50, 90);
-            //const data = new Date();
-            const equip_id = 1;
-
-            //valores aceitos: -2, -1, 0, 1, 2, 3, 4, 5
-            const param_minimo = -2; //-3
-            const param_max = 5;
-
-            // Instrução SQL
-            const sql = `INSERT INTO novo_equipamento_dados (dados_temp, dados_umid, equip_id) VALUES (?, ?, ?)`;
-
-            // Definição dos dados a serem inseridos em um array
-            const values = [dados_temp, dados_umid, equip_id,];
-
-            //Execução da instrução sql passando os parâmetros
-            const execSql = await db.query(sql, values); 
-
-            //pega a linha que foi inserida
-            const dados_id = execSql[0].insertId;   
-
-            if (dados_temp >= param_minimo && dados_temp <= param_max) {
-
-                console.log('Valor dentro do intervalo', dados_temp);
-
-            } else {
-                console.log('Emitiu alerta: ', dados_temp);
-
-                if(dados_temp < param_minimo) {
-
-                    //temperatura menor
-                    const alerta_id = 2;
-
-                    emitirAlerta(equip_id, alerta_id, dados_id)
-
-                } else {
-
-                    //temperatura maior
-                    const alerta_id = 1;
-
-                    emitirAlerta(equip_id, alerta_id, dados_id)
-
-                }
-                
-            }
-
-        //}
-        
-    } catch (error) {
-        console.log(error);
-    }
-}
-
 //grava um alerta
 async function emitirAlerta(equip_id, alerta_id, dados_id) {
     try { 
@@ -212,3 +142,20 @@ function getRandomInRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+
+const interval = setInterval(async () => {
+    n++;
+    //console.log('repete' + ' ' + n);
+    //cadastrar();
+    const dados_temp = parseFloat(lerTemperaturaSimulada().toFixed(2));
+    const dados_umid = lerUmidadeSimulada();
+    const dados_data = await lerDataSimulada(1);
+
+    //console.log(dados_data);
+
+    novoCadastrar(dados_temp, dados_umid, equip_id, dados_data);
+
+}, 10000); // 1800000 milissegundos = 30 minutos
+
+
+interval;
